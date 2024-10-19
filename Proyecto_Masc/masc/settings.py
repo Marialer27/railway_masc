c160fad182fda59ab23e6463503bd668379b7a0c
Marialer27 <maria.alerodriguez@gmail.com>
2024-10-19 18:19:09 -0300

    allowedhosts

--- Proyecto_Masc/masc/settings.py
+++ Proyecto_Masc/masc/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','https://railwaymasc-production.up.railway.app/']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME: ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
